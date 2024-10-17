2ce6793fb295261f69b5338545910d5b8c2204fd
Juan Vela <spartanjuanv@gmail.com>
2024-10-16 19:29:37 -0500

    primer intento de mostrar entrenadores disponibles

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -11,6 +11,7 @@ namespace NET_MVC.Controllers
     [Authorize]
     public class ClienteController : Controller
     {
+        public OracleConnection conexionBD = Conexion.GetConnection();
         AdmCliente consulta = new AdmCliente();
         public IActionResult Registrar()
         {
@@ -24,7 +25,8 @@ namespace NET_MVC.Controllers
 
         public IActionResult AsignarEntrenadorCliente()
         {
-            return View("AsignarEntrenadorCliente");
+            List<EntrenadorModel> entrenadores = ObtenerEntrenadoresDisponibles();
+            return View("AsignarEntrenadorCliente", entrenadores);
         }
 
         [HttpPost]
@@ -89,7 +91,32 @@ namespace NET_MVC.Controllers
             bool clienteExiste = consulta.ClienteExiste(identificacion);
             return Json(new { existe = clienteExiste });
         }
+        private List<EntrenadorModel> ObtenerEntrenadoresDisponibles()
+        {
+            var entrenadores = new List<EntrenadorModel>();
 
+            if (Conexion.abrirConexion())
+            {
+                string sql = "SELECT ID_ENTRENADOR, MAX(NOMBRE_ENTRENADOR) NOMBRE_ENTRENADOR ,MAX(nombre_especialidad) AREA_ESPECIALIDAD, COUNT(*) CLIENTES_ASIGNADOS FROM ENTRENADOR NATURAL JOIN areaespecialidad NATURAL JOIN CLIENTE GROUP BY id_entrenador HAVING COUNT(*) < 5";
+
+                using (OracleCommand cmd = new OracleCommand(sql, conexionBD))
+                {
+                    OracleDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        EntrenadorModel entrenador = new EntrenadorModel
+                        {
+                            Id = reader["ID_ENTRENADOR"].ToString(),
+                            Nombre = reader["NOMBRE"].ToString(),
+                            Especialidad = reader["AREA_ESPECIALIDAD"].ToString(),
+                            ClientesAsignados = Convert.ToInt32(reader["CLIENTES_ASIGNADOS"])
+                        };
+                        entrenadores.Add(entrenador);
+                    }
+                }
+            }
+            return entrenadores;
+        }
     }
 }
 
--- Models/EntrenadorModel.cs
+++ Models/EntrenadorModel.cs
@@ -7,12 +7,12 @@ namespace NET_MVC.Models
         public required string Id { get; set; }
         public string? IdSede { get; set; }
         public required string Nombre { get; set; }
-        public required string Genero { get; set; }
-        public required string Telefono { get; set; }
+        public string Genero { get; set; }
+        public string Telefono { get; set; }
         public required string Especialidad { get; set; }
-        public required string Salario { get; set; }
-        public required DateTime fechaInicioContrato { get; set; }
-        public required string Contraseña { get; set; }
+        public string Salario { get; set; }
+        public DateTime fechaInicioContrato { get; set; }
+        public string Contraseña { get; set; }
         public required int ClientesAsignados { get; set; }
 
     }
