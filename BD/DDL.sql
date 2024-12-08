52dace4267df40d81b5a4a46d44602b67ccf37aa
paulamunoz06 <paulamunoz@unicauca.edu.co>
2024-12-08 18:19:49 -0500

    BD
    Segunda versión

--- BD/DDL.sql
+++ BD/DDL.sql
@@ -8,14 +8,13 @@ DROP TABLE CuentaEntrenador;
 DROP TABLE Entrenador;
 DROP TABLE AreaEspecialidad;
 DROP TABLE CuentaSede;
-DROP TABLE Sede;
 DROP TABLE NombreEjercicio;
+DROP TABLE Persona;
+DROP TABLE Sede;
 
+-- BASES DE DATOS 
 
--- BASES DE DATOS
-
-
-CREATE TABLE Sede
+CREATE TABLE Sede 
 (
     id_Sede NUMBER NOT NULL,
     nombre_sede VARCHAR2(20) NOT NULL,
@@ -24,136 +23,125 @@ CREATE TABLE Sede
     CONSTRAINT pk_Sede PRIMARY KEY(id_Sede)
 );
 
-
 CREATE TABLE CuentaSede
 (
-    id_Sede NUMBER NOT NULL,  
-    password_sede VARCHAR2(20) NOT NULL,
+    id_Sede NUMBER NOT NULL,   
+    password_sede VARCHAR2(20) NOT NULL, 
     CONSTRAINT pk_CuentaSede PRIMARY KEY(id_Sede),
     CONSTRAINT fk_SedeCuenta FOREIGN KEY (id_Sede) REFERENCES Sede(id_Sede)
 );
 
+CREATE TABLE Persona
+(
+    id_Persona NUMBER NOT NULL,
+    nombre_Persona VARCHAR2(50) NOT NULL,   
+    telefono_Persona VARCHAR2(20) NOT NULL, 
+    genero_Persona CHAR(2) NOT NULL,  
+    id_Sede NUMBER NOT NULL,
+    CONSTRAINT pk_Persona PRIMARY KEY(id_Persona),
+    CONSTRAINT ck_genero_Persona CHECK (genero_Persona IN ('M', 'F', 'NE')),
+    CONSTRAINT fk_SedePersona FOREIGN KEY (id_Sede) REFERENCES Sede(id_Sede)
+);
 
 CREATE TABLE AreaEspecialidad
 (
-    id_Area_Especialidad NUMBER NOT NULL,
-    nombre_especialidad VARCHAR2(20) NOT NULL,
-    CONSTRAINT pk_AreaEspecialidad PRIMARY KEY(id_Area_Especialidad),
-    CONSTRAINT ck_nombre_especialidad CHECK (nombre_especialidad IN ('Crossfit', 'Fuerza', 'Reduccion de peso', 'Culturismo'))
+    id_AE NUMBER NOT NULL,
+    nombre_AE VARCHAR2(20) NOT NULL,
+    CONSTRAINT pk_AreaEspecialidad PRIMARY KEY(id_AE),
+    CONSTRAINT ck_nombre_especialidad CHECK (nombre_AE IN ('Crossfit', 'Fuerza', 'Reduccion de peso', 'Culturismo'))
 );
-
 
 CREATE TABLE Entrenador
 (
     id_Entrenador NUMBER NOT NULL,
-    id_Sede NUMBER NOT NULL,
-    id_Area_Especialidad NUMBER NOT NULL,
-    nombre_entrenador VARCHAR2(50) NOT NULL,  
-    telefono_entrenador VARCHAR2(20) NOT NULL,
-    genero_entrenador CHAR(2) NOT NULL,  
+    id_AE NUMBER NOT NULL,
     CONSTRAINT pk_Entrenador PRIMARY KEY(id_Entrenador),
-    CONSTRAINT fk_SedeEntrenador FOREIGN KEY (id_Sede) REFERENCES Sede(id_Sede),
-    CONSTRAINT fk_AreaEspeEntrenador FOREIGN KEY (id_Area_Especialidad) REFERENCES AreaEspecialidad(id_Area_Especialidad),
-    CONSTRAINT ck_genero_entrenador CHECK (genero_entrenador IN ('M', 'F', 'NE'))
+    CONSTRAINT fk_PersonaEntrenador FOREIGN KEY (id_Entrenador) REFERENCES Persona(id_Persona),
+    CONSTRAINT fk_AEEntrenador FOREIGN KEY (id_AE) REFERENCES AreaEspecialidad(id_AE)
 );
-
 
 CREATE TABLE CuentaEntrenador
 (
-    id_Entrenador NUMBER NOT NULL,  
-    password_entrenador VARCHAR2(20) NOT NULL,
+    id_Entrenador NUMBER NOT NULL,   
+    password_entrenador VARCHAR2(20) NOT NULL, 
     CONSTRAINT pk_CuentaEntrenador PRIMARY KEY(id_Entrenador),
     CONSTRAINT fk_EntrenadorCuenta FOREIGN KEY (id_Entrenador) REFERENCES Entrenador(id_Entrenador)
 );
-
 
 CREATE TABLE Contrato
 (
     id_entrenador NUMBER NOT NULL,
     salario NUMBER NOT NULL,
-    fecha_inicio_contrato DATE NOT NULL,
+    fecha_inicio_contrato DATE NOT NULL, 
     fecha_fin_contrato DATE,
     CONSTRAINT pk_Contrato PRIMARY KEY(id_entrenador),
     CONSTRAINT fk_EntrenadorContrato FOREIGN KEY (id_entrenador) REFERENCES Entrenador(id_entrenador)
 );
 
-
-CREATE TABLE Cliente
+CREATE TABLE Cliente 
 (
     id_Cliente NUMBER NOT NULL,
-    id_Sede NUMBER NOT NULL,
     id_Entrenador NUMBER, -- acepta nulos para los clientes no premium
-    nombre_cliente VARCHAR2(50) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    telefono_cliente VARCHAR2(20) NOT NULL,
-    genero_cliente CHAR(2) NOT NULL,  
+    fecha_nacimiento DATE NOT NULL, 
     CONSTRAINT pk_Cliente PRIMARY KEY(id_Cliente),
-    CONSTRAINT fk_SedeCliente FOREIGN KEY (id_Sede) REFERENCES Sede(id_Sede),
-    CONSTRAINT fk_EntrenadorCliente FOREIGN KEY (id_Entrenador) REFERENCES Entrenador(id_Entrenador),
-    CONSTRAINT ck_genero_cliente CHECK (genero_cliente IN ('M', 'F', 'NE'))
+    CONSTRAINT fk_PersonaCliente FOREIGN KEY (id_Cliente) REFERENCES Persona(id_Persona),
+    CONSTRAINT fk_EntrenadorCliente FOREIGN KEY (id_Entrenador) REFERENCES Entrenador(id_Entrenador)
 );
 
-
-CREATE TABLE Membresia
+CREATE TABLE Membresia 
 (
     id_Cliente NUMBER NOT NULL,
     fecha_suscripcion DATE NOT NULL,
     tipo VARCHAR2(10) NOT NULL,  -- Tipo de membresia
-    CONSTRAINT ck_tipo_membresia CHECK (tipo IN ('General', 'Premium', 'Inactivo')), -- CHECK en l?nea separada
+    CONSTRAINT ck_tipo_membresia CHECK (tipo IN ('General', 'Premium', 'Inactivo')), -- CHECK en l nea separada
     CONSTRAINT pk_Membresia PRIMARY KEY(id_Cliente),
     CONSTRAINT fk_ClienteMembresia FOREIGN KEY (id_Cliente) REFERENCES Cliente(id_Cliente)
 );
 
 
-
-
 CREATE TABLE ProgresoFisico
 (
     id_Cliente NUMBER NOT NULL,          
-    fecha_toma DATE NOT NULL,          
+    fecha_toma DATE NOT NULL,           
     peso NUMBER NOT NULL,                
     estatura NUMBER NOT NULL,
-    presion_arterial_reposo VARCHAR2(7) NOT NULL,
+    presion_arterial_reposo VARCHAR2(7) NOT NULL, 
     CONSTRAINT pk_ProgresoFisico PRIMARY KEY(id_Cliente, fecha_toma),  
     CONSTRAINT fk_ClienteProgreso FOREIGN KEY (id_Cliente) REFERENCES Cliente(id_Cliente)
 );
 
-
-CREATE TABLE PMF
+CREATE TABLE PMF 
 (
-    id_Cliente NUMBER NOT NULL,        
-    fecha_valoracion DATE NOT NULL,    
+    id_Cliente NUMBER NOT NULL,         
+    fecha_valoracion DATE NOT NULL,     
     objetivo VARCHAR2(100) NOT NULL,    
-    CONSTRAINT pk_PMF PRIMARY KEY(id_Cliente, fecha_valoracion),
-    CONSTRAINT fk_ClientePMF FOREIGN KEY (id_Cliente) REFERENCES Cliente(id_Cliente)
+    CONSTRAINT pk_PMF PRIMARY KEY(id_Cliente, fecha_valoracion), 
+    CONSTRAINT fk_ClientePMF FOREIGN KEY (id_Cliente) REFERENCES Cliente(id_Cliente) 
 );
-
 
 CREATE TABLE NombreEjercicio
 (
-    id_nombre_ejercicio NUMBER NOT NULL,
+    id_nombre_ejercicio NUMBER NOT NULL, 
     nombre_ejercicio VARCHAR2(30) NOT NULL, -- mayor tamaño para el nombre
     CONSTRAINT pk_NombreEjercicio PRIMARY KEY(id_nombre_ejercicio),
     CONSTRAINT ck_NombreEjercicio CHECK (nombre_ejercicio IN ('Squad', 'Leg press', 'Bench press', 'Pull-ups','Bicep curls', 'Hip thrust', 'Deadlifts','Press banca', 'Planks', 'Bulgarian split squats'))
 );
 
-
-CREATE TABLE Ejercicio
+CREATE TABLE Ejercicio 
 (
     id_ejercicio NUMBER NOT NULL,      
-   
-    id_cliente NUMBER NOT NULL,                      
+    
+    id_cliente NUMBER NOT NULL,                       
     id_fecha_valoracion DATE NOT NULL,  
-   
+    
     id_nombre_ejercicio NUMBER NOT NULL,  
-   
+    
     numero_repeticiones NUMBER NOT NULL,              
-   
-    numero_series NUMBER NOT NULL,                    
-    dia_semana VARCHAR2(10) NOT NULL,  
+    
+    numero_series NUMBER NOT NULL,                     
+    dia_semana VARCHAR2(10) NOT NULL,   
     CONSTRAINT pk_Ejercicio PRIMARY KEY(id_ejercicio),  
-    CONSTRAINT fk_PMF_Ejercicio FOREIGN KEY (id_cliente, id_fecha_valoracion) REFERENCES PMF(id_cliente, fecha_valoracion),
+    CONSTRAINT fk_PMF_Ejercicio FOREIGN KEY (id_cliente, id_fecha_valoracion) REFERENCES PMF(id_cliente, fecha_valoracion), 
     CONSTRAINT fk_NombreEjercicio FOREIGN KEY (id_nombre_ejercicio) REFERENCES NombreEjercicio(id_nombre_ejercicio),
     CONSTRAINT ck_dia_semana CHECK (dia_semana IN ('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'))
 );
-
--- BD/FP.sql
+++ BD/FP.sql
@@ -185,3 +185,79 @@ EXCEPTION
     WHEN OTHERS THEN
         RAISE;
 END calcular_dias_restantes;
+
+--PROCEDIMIENTOS USADOS PARA FUNCIONAMIENTO DE INSERTAR PMF
+
+CREATE OR REPLACE PROCEDURE crearPMF (
+    p_id_cliente IN pmf.id_Cliente%type, 
+    p_fecha_valoracion IN pmf.fecha_valoracion%type)
+IS
+BEGIN
+    INSERT INTO pmf VALUES (p_id_cliente, p_fecha_valoracion);
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE = -1 THEN
+            RAISE_APPLICATION_ERROR(-20010, 'PMF existente para el cliente con la fecha de valoracion diligenciada');
+        ELSIF SQLCODE = -2291 THEN
+            RAISE_APPLICATION_ERROR(-20011, 'Cliente no existente');
+        ELSIF SQLCODE = -1400 THEN
+            RAISE_APPLICATION_ERROR(-20005, 'Valores nulos no permitidos en una columna que no acepta nulos');
+        ELSIF SQLCODE = -12899 THEN 
+            RAISE_APPLICATION_ERROR(-20006, 'Error de restricci?n de longitud de campo');
+        ELSIF SQLCODE = -1722 THEN 
+            RAISE_APPLICATION_ERROR(-20007, 'Error de conversi?n de tipos de datos');
+        ELSE
+            RAISE_APPLICATION_ERROR(-20008, 'Error no identificado: ' || SQLERRM);
+        END IF;
+END;
+
+--PROCEDIMIENTOS USADOS PARA FUNCIONAMIENTO DE INSERTAR EJERCICIO
+
+--Secuencia
+
+DROP SEQUENCE seq_idejercicio;
+
+CREATE SEQUENCE seq_idejercicio
+START WITH 1
+INCREMENT BY 1
+MINVALUE 1
+NOCYCLE;
+
+--Trigger secuencia
+CREATE OR REPLACE TRIGGER trg_incrementar_id_ejercicio
+BEFORE INSERT ON Ejercicio
+FOR EACH ROW
+BEGIN
+    :NEW.id_ejercicio := seq_idejercicio.NEXTVAL;
+END; 
+---------------------- CREAR EJERCICIO -----------------------------------------
+CREATE OR REPLACE PROCEDURE crearEjercicio (     
+    p_id_cliente IN ejercicio.id_cliente%type,                      
+    p_id_fecha_valoracion IN ejercicio.id_fecha_valoracion%type,  
+    p_id_nombre_ejercicio IN ejercicio.id_nombre_ejercicio%type,  
+    p_numero_repeticiones IN ejercicio.numero_repeticiones%type,              
+    p_numero_series IN ejercicio.numero_series%type,                    
+    p_dia_semana IN ejercicio.dia_semana%type)  
+IS
+    id_ejercicio NUMBER;
+BEGIN
+    INSERT INTO ejercicio VALUES (id_ejercicio,p_id_cliente,p_id_fecha_valoracion,p_id_nombre_ejercicio,p_numero_repeticiones,p_numero_series,p_dia_semana);
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE = -1 THEN
+            RAISE_APPLICATION_ERROR(-20013, 'Ejercicio existente');
+        ELSIF SQLCODE = -2291 THEN
+            RAISE_APPLICATION_ERROR(-20014, 'Nombre del ejercicio no existente');
+        ELSIF SQLCODE = -290 THEN
+            RAISE_APPLICATION_ERROR(-20015, 'Dia de la semana no existente');
+        ELSIF SQLCODE = -1400 THEN
+            RAISE_APPLICATION_ERROR(-20005, 'Valores nulos no permitidos en una columna que no acepta nulos');
+        ELSIF SQLCODE = -12899 THEN 
+            RAISE_APPLICATION_ERROR(-20006, 'Error de restricci?n de longitud de campo');
+        ELSIF SQLCODE = -1722 THEN 
+            RAISE_APPLICATION_ERROR(-20007, 'Error de conversi?n de tipos de datos');
+        ELSE
+            RAISE_APPLICATION_ERROR(-20008, 'Error no identificado: ' || SQLERRM);
+        END IF;
+END;
+
